6a211c0b5539d05763e57316467ec7fe38c9e118
amletoitz <mf715346@gmail.com>
2026-04-15 11:06:23 -0300

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,12 @@
 # ================================
 # SENAC Easy — Entry point WSGI
-# Usado pelo Gunicorn em produção
 # ================================
+
+import sys
+import os
+
+# Adiciona o diretório atual no path do Python
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from api_groq import app
 
